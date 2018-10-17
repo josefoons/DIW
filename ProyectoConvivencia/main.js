bcaced73a9cf4864f7a608bfeabb17b8c2d42e0b
Jose Fons <josefoons@gmail.com>
2018-10-17 18:31:57 +0200

    Update Form with CSS and JS

--- ProyectoConvivencia/main.js
+++ ProyectoConvivencia/main.js
@@ -1,6 +1,18 @@
-function onSubmit( form ){
-    
-    var data = JSON.stringify( $(form).serializeArray() ); //  <-----------
-    console.log( data );
-    return false; //don't submit
+function onSubmit(form) {
+
+  var data = JSON.stringify($(form).serializeArray()); //  <-----------
+  console.log(data);
+  return false; //don't submit
+}
+
+function showDiv(elem){
+  if(elem.value == 1) {
+    document.getElementById('leve').style.display = "block";
+    document.getElementById('grave').style.display = "none";
+  } else {
+    if(elem.value == 2) {
+      document.getElementById('leve').style.display = "none";
+      document.getElementById('grave').style.display = "block";
+    }
   }
+}
