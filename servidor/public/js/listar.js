5f43dd39ad8fd21e40514043d0e0060aa7c56e1c
Jose Fons <josefoons@gmail.com>
2018-12-05 19:00:44 +0100

    Upload more

--- servidor/public/js/listar.js
+++ servidor/public/js/listar.js
@@ -16,7 +16,7 @@ function eliminarIncidencia(elemento) {
 $.getJSON("http://localhost:3000/minionsL", function (data) {
   var items = [];
   $.each(data, function (key, val) {
-    items.push("<li id='" + key + "' class='list-group-item'>" + val.nombreAlumno + " <input id='" + val._id + "' onclick='modificarL(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'></li>");
+    items.push("<li id='" + key + "' class='list-group-item'>" + val.nombreAlumno + " | " + val.nombreProfesor + " | " + val.fechaHoy +  "<input id='" + val._id + "' onclick='modificarL(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'></li>");
   });
 
   $("<ul/>", {
@@ -29,7 +29,7 @@ $.getJSON("http://localhost:3000/minionsL", function (data) {
 $.getJSON("http://localhost:3000/minionsG", function (data) {
   var items = [];
   $.each(data, function (key, val) {
-    items.push("<li id='" + key + "' class='list-group-item'>" + val.nombreAlumno + " <input id='" + val._id + "' onclick='modificarG(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'></li>");
+    items.push("<li id='" + key + "' class='list-group-item'>" + val.nombreAlumno + " | " + val.nombreProfesor + " | " + val.fechaHoy +  " <input id='" + val._id + "' onclick='modificarG(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'></li>");
   });
 
   $("<ul/>", {
--- servidor/public/js/main.js
+++ servidor/public/js/main.js
@@ -1,3 +1,8 @@
+window.addEventListener("load", cargar, false);
+function cargar() {
+    fechaHoy();
+}
+
 function showDiv(elem) {
   if (elem.value == "leve") {
     document.getElementById('leve').style.display = "block";
@@ -10,3 +15,20 @@ function showDiv(elem) {
   }
 }
 
+function fechaHoy() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+  
+  if(dd<10) {
+      dd = '0'+dd
+  } 
+  
+  if(mm<10) {
+      mm = '0'+mm
+  } 
+  
+  today = dd + '/' + mm + '/' + yyyy;
+  document.getElementById("fechaFirma").value = today;
+}
