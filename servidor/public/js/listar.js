1dda6ea129d76d50d85e9224fa52213397d10c9c
Jose Fons <josefoons@gmail.com>
2018-12-13 19:01:47 +0100

    Añadir Scroll lateral en listar

--- servidor/public/js/listar.js
+++ servidor/public/js/listar.js
@@ -16,7 +16,7 @@ function eliminarIncidencia(elemento) {
 $.getJSON("http://localhost:3000/minionsL", function (data) {
   var items = [];
   $.each(data, function (key, val) {
-    items.push("<li id='" + key + "' class='list-group-item'>" + val.nombreAlumno + " | " + val.nombreProfesor + " | " + val.fechaHoy +  "<input id='" + val._id + "' onclick='modificarL(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'><input type='button' style='float:right;' data-toggle='modal' id='" + val._id + "#L' data-target='#myModal' onclick='realizaVisto(this.id)' value='CHECK' /></li>");
+    items.push("<li id='" + key + "' class='list-group-item incidenciasLeve'>" + val.nombreAlumno + " | " + val.nombreProfesor + " | " + val.fechaHoy +  "<input id='" + val._id + "' onclick='modificarL(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'><input type='button' style='float:right;' data-toggle='modal' id='" + val._id + "#L' data-target='#myModal' onclick='realizaVisto(this.id)' value='CHECK' /></li>");
   });
 
   $("<ul/>", {
@@ -29,7 +29,7 @@ $.getJSON("http://localhost:3000/minionsL", function (data) {
 $.getJSON("http://localhost:3000/minionsG", function (data) {
   var items = [];
   $.each(data, function (key, val) {
-    items.push("<li id='" + key + "' class='list-group-item'>" + val.nombreAlumno + " | " + val.nombreProfesor + " | " + val.fechaHoy +  "<input id='" + val._id + "' onclick='modificarG(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'><input type='button' style='float:right;' data-toggle='modal' id='" + val._id + "#G' data-target='#myModal' onclick='realizaVisto(this.id)' value='CHECK' /></li>");
+    items.push("<li id='" + key + "' class='list-group-item incidenciasGrave'>" + val.nombreAlumno + " | " + val.nombreProfesor + " | " + val.fechaHoy +  "<input id='" + val._id + "' onclick='modificarG(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'><input type='button' style='float:right;' data-toggle='modal' id='" + val._id + "#G' data-target='#myModal' onclick='realizaVisto(this.id)' value='CHECK' /></li>");
   });
 
   $("<ul/>", {
@@ -53,4 +53,32 @@ function checkG(elemento) {
 
 function checkL(elemento) {
   location.href='http://localhost:3000/visto.html?' + elemento + "?L";
+}
+
+function buscarGrave() {
+  var input = document.getElementById("buscadorGrave");
+  var filter = input.value.toLowerCase();
+  var nodes = document.getElementsByClassName('incidenciasGrave');
+
+  for (i = 0; i < nodes.length; i++) {
+    if (nodes[i].innerText.toLowerCase().includes(filter)) {
+      nodes[i].style.display = "block";
+    } else {
+      nodes[i].style.display = "none";
+    }
+  }
+}
+
+function buscarLeve() {
+  var input = document.getElementById("buscadorLeve");
+  var filter = input.value.toLowerCase();
+  var nodes = document.getElementsByClassName('incidenciasLeve');
+
+  for (i = 0; i < nodes.length; i++) {
+    if (nodes[i].innerText.toLowerCase().includes(filter)) {
+      nodes[i].style.display = "block";
+    } else {
+      nodes[i].style.display = "none";
+    }
+  }
 }
