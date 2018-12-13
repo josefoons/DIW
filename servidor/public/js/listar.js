509daabbe8bba82e7d91d36a4ebc8f6dfae165cc
Jose Fons <josefoons@gmail.com>
2018-12-13 18:28:17 +0100

    Corregir Error en editar las Graves.

--- servidor/public/js/listar.js
+++ servidor/public/js/listar.js
@@ -29,7 +29,7 @@ $.getJSON("http://localhost:3000/minionsL", function (data) {
 $.getJSON("http://localhost:3000/minionsG", function (data) {
   var items = [];
   $.each(data, function (key, val) {
-    items.push("<li id='" + key + "' class='list-group-item'>" + val.nombreAlumno + " | " + val.nombreProfesor + " | " + val.fechaHoy +  "<input id='" + val._id + "' onclick='modificarL(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'><input type='button' style='float:right;' data-toggle='modal' id='" + val._id + "#G' data-target='#myModal' onclick='realizaVisto(this.id)' value='CHECK' /></li>");
+    items.push("<li id='" + key + "' class='list-group-item'>" + val.nombreAlumno + " | " + val.nombreProfesor + " | " + val.fechaHoy +  "<input id='" + val._id + "' onclick='modificarG(this.id)' type='button' value='MODIFICAR' style='float:right;'><input onclick='eliminarIncidencia(this.id)' id='" + val._id + "' type='button' value='ELIMINAR' style='float:right;'><input type='button' style='float:right;' data-toggle='modal' id='" + val._id + "#G' data-target='#myModal' onclick='realizaVisto(this.id)' value='CHECK' /></li>");
   });
 
   $("<ul/>", {
